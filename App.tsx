91d065d3b3728989b3e60869bed3b33f1f81ebdd
Brian Rivera <alexisrb2408@gmail.com>
2026-07-17 12:02:47 -0500

    Fix: polyfill Appearance.setColorScheme for web

--- App.tsx
+++ App.tsx
@@ -7,6 +7,11 @@ import { NavigationContainer, NavigationContainerRef } from '@react-navigation/n
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StoreProvider, useStore } from './src/store/sunatStore'
 
+// Polyfill: setColorScheme no existe en react-native-web
+if (typeof Appearance.setColorScheme !== 'function') {
+  ;(Appearance as any).setColorScheme = () => {}
+}
+
 import LoginScreen from './src/screens/LoginScreen'
 import RegisterScreen from './src/screens/RegisterScreen'
 import HomeScreen from './src/screens/HomeScreen'
